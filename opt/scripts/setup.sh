f6d11965ef4804bf047b1120fa79735ff2da3a5d
Keren Dong <dongkeren@gmail.com>
2021-02-25 17:23:07 +0800

    Test hosts

--- opt/scripts/setup.sh
+++ opt/scripts/setup.sh
@@ -33,11 +33,13 @@ localedef -c -f GB18030 -i zh_CN zh_CN.GB18030
 echo "source /opt/rh/devtoolset-9/enable" >> /etc/profile
 echo "source /opt/rh/devtoolset-9/enable" >> /etc/bashrc
 
+source /opt/rh/devtoolset-9/enable
+
 ln -s /usr/bin/cmake3 /usr/bin/cmake
 
 mkdir /tmp/python
 cd /tmp/python
-wget https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tgz
+wget --no-verbose https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tgz
 tar xzf Python-3.7.7.tgz
 cd Python-3.7.7
 ./configure --with-ensurepip=install --enable-optimizations --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
@@ -48,3 +50,4 @@ rm -rf /tmp/python
 pip3 install pipenv==2020.11.15
 
 echo "127.0.0.1 local-test" >> /etc/hosts
+cat /etc/hosts
