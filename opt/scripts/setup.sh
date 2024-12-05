a593cab30b59f341732ea1568b2d8a03f908cc29
yizhi <zhangyiizhi@126.com>
2024-12-05 12:48:55 +0800

    [fix] try fix centos

--- opt/scripts/setup.sh
+++ opt/scripts/setup.sh
@@ -2,7 +2,6 @@
 
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-* 
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-* 
-yum -y update && yum clean all
 
 set -e
 
